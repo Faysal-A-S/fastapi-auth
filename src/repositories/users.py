fd59003e3f09f936d439b1cdf27b691666b19c2c
Faysal-A-S <faysala1655@gmail.com>
2024-11-17 16:42:52 +0600

    refactor: spacing change

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -7,11 +7,10 @@ from sqlalchemy.orm import Session
 
 class UserRepo(BaseRepo[User, UserIn, UserUpdate]):
 
-    def search_by_phone(self, db:Session, phone_in:str) -> Optional[User]:
+    def search_by_phone(self, db: Session, phone_in: str) -> Optional[User]:
         return db.query(self.model).filter(self.model.phone == phone_in).first()
 
-
-    def search_by_email(self, db:Session, email_in:str) -> Optional[User]:
+    def search_by_email(self, db: Session, email_in: str) -> Optional[User]:
         return db.query(self.model).filter(self.model.email == email_in).first()
 
 
