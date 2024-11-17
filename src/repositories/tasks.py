2220e8e2e77a985aecf7de8bba72d3cee886c11e
Faysal-A-S <faysala1655@gmail.com>
2024-11-17 16:41:28 +0600

    fix: create function remove

--- src/repositories/tasks.py
+++ src/repositories/tasks.py
@@ -6,21 +6,16 @@ from sqlalchemy.orm import Session
 
 class TaskRepo(BaseRepo[Task, TaskIn, TaskUpdate]):
 
-    def create_task(self, db: Session, data_in: TaskIn, user_id: int):
-        data_for_db = TaskIn(user_id=user_id, **data_in.dict())
-        task = self.create(db=db, data_in=data_for_db)
-        return task
-
     def get_tasks(self, db: Session, current_user_id: int):
         query = db.query(self.model).filter(
             self.model.user_id == current_user_id).all()
         return query
 
-    def get_one_task(self, db: Session, current_user_id: int, id: int):
+    def get_one_task(self, db: Session, id: int):
         query = db.query(self.model).filter(self.model.id == id).first()
         return query
 
-    def delete_task(self, db: Session, id: int, current_user_id: int):
+    def delete_task(self, db: Session, id: int):
 
         result = db.query(self.model).filter(self.model.id ==
                                              id).delete(synchronize_session=False)
