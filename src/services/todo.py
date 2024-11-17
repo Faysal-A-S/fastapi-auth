1af10903fe2ad0becdc4be241e7b4c3ee652a4dc
Faysal-A-S <faysala1655@gmail.com>
2024-11-17 16:46:57 +0600

    refactor: duplicate code remove

--- src/services/todo.py
+++ src/services/todo.py
@@ -1,22 +1,21 @@
-from services import BaseService
-from repositories import todo_repo
-from models import ToDo
-from schemas import TodoIn, TodoUpdate
+from schemas import TodoIn, TodoDBIn, TodoUpdate
 from sqlalchemy.orm import Session
-from exceptions import ServiceResult, AppException
-from fastapi import status
+from repositories import todo_repo
+from services import BaseService
+from models import ToDo
 
 
 class TodoService(BaseService[ToDo, TodoIn, TodoUpdate]):
 
-    def create_todo(self, db:Session, data_in:TodoIn, user_id:int):
-        todo = self.repo.create_todo(db=db, data_in=data_in, user_id=user_id)
+    def create_todo(self, db: Session, data_in: TodoIn, user_id: int):
+        todo = self.create(
+            db=db,
+            data_in=TodoDBIn(
+                **data_in.dict(), user_id=user_id
+            )
+        )
 
-        if not todo:
-            return ServiceResult(AppException.ServerError("Todo not created!"))
-        else:
-            return ServiceResult(todo, status_code=status.HTTP_201_CREATED)
-
+        return todo
 
 
 todo_service = TodoService(ToDo, todo_repo)
