ec92309a75caf546e179476a3772bd664a9600be
Faysal-A-S <faysala1655@gmail.com>
2024-11-17 16:44:11 +0600

    feat: tododbin class add

--- src/schemas/todo.py
+++ src/schemas/todo.py
@@ -3,29 +3,33 @@ from typing import Optional
 
 
 class TodoBase(BaseModel):
-       title: str
-       task: Optional[str] = None
+    title: str
+    task: Optional[str] = None
 
 
 class TodoIn(TodoBase):
-       user_id: Optional[int] = None
-       
+    pass
+
+
+class TodoDBIn(TodoBase):
+    user_id: int
+
 
 class TodoOut(TodoBase):
-       user_id: int
-       id: int
+    user_id: int
+    id: int
 
-       class Config:
-              orm_mode = True
+    class Config:
+        orm_mode = True
 
 
 class TodoOutUser(TodoBase):
-       id: int
+    id: int
 
-       class Config:
-              orm_mode = True
+    class Config:
+        orm_mode = True
 
 
 class TodoUpdate(BaseModel):
-       title: str
-       task: Optional[str] = None
+    title: str
+    task: Optional[str] = None
